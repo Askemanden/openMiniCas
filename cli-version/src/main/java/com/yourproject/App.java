f4d090be2ca039b1df870dfbc1e9eeda24c498a4
Askemanden <askevandsoaremark@gmail.com>
2025-05-09 19:52:36 +0200

    initial commit

---
+++ cli-version/src/main/java/com/yourproject/App.java
@@ -0,0 +1,13 @@
+package com.yourproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
